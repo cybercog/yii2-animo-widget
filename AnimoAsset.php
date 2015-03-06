996dc2e40530d285d9ce3da91b83facc11b13fb2
xjflyttp <xjflyttp@gmail.com>
2015-03-06 16:05:43 +0800

    update

--- AnimoAsset.php
+++ AnimoAsset.php
@@ -12,25 +12,10 @@ use yii\web\AssetBundle;
 
 class AnimoAsset extends AssetBundle {
 
-    public $sourcePath;
+    public $sourcePath = '@vendor/xj/yii2-animo-widget/assets';
     public $basePath = '@webroot/assets';
-    public $publishOptions = ['forceCopy' => YII_DEBUG];
     public $css = ['animate+animo.css'];
-    public $js = [];
+    public $js = ['animo.js'];
     public $depends = ['yii\web\JqueryAsset'];
-
-    private function getJs() {
-        return [
-            YII_DEBUG ? 'animo.js' : 'animo.min.js',
-        ];
-    }
-
-    public function init() {
-        $this->sourcePath = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'assets';
-        if (empty($this->js)) {
-            $this->js = $this->getJs();
-        }
-        return parent::init();
-    }
 
 }
